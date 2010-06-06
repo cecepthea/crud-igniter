4ebe20bf1142375b17b9f767c4b05d5dd28f72bd
thiagophx <thiagophx@f1d8aff6-6735-310d-87d8-994824499a75>
2010-06-06 23:58:44 +0000

    Deleting invalid tag

--- tags/CrudIgniter_1.0/templates/controllers/includes/add_default.inc.php
+++
@@ -1,7 +0,0 @@
-$data = array();
-<?php foreach ($table['Fields'] as $field) { ?>
-		$data['<?php echo $field['Field']; ?>'] = $this->input->post('<?php echo $field['Field']; ?>');
-<?php } ?>
-
-		$this-><?php echo strtolower($table['Name']); ?>_model->insert($data);
-		redirect('<?php echo strtolower($table['Name']); ?>/');
--- tags/CrudIgniter_1.0/templates/controllers/includes/edit_default.inc.php
+++
@@ -1,7 +0,0 @@
-		
-<?php foreach ($table['Fields'] as $field) { ?>
-		$data['<?php echo $field['Field']; ?>'] = $this->input->post('<?php echo $field['Field']; ?>');
-<?php } ?>
-			
-		$this-><?php echo strtolower($table['Name']); ?>_model->update($data, $id);		
-		redirect('<?php echo strtolower($table['Name']); ?>/');
